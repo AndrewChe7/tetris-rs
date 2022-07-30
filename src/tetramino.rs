e8c9656ed4f0517307c14bac603fac8bcb0cbfb7
AndrewChe <Chernjakov2@gmail.com>
2022-07-30 21:59:07 +0300

    Add a lot of stuff

--- src/tetramino.rs
+++ src/tetramino.rs
@@ -1,57 +1,58 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, core::FixedTimestep};
 use crate::tilemap::*;
 use rand::Rng;
 
-const I_TETRAMINO: [[usize; 4]; 4] = [
+const I_TETRAMINO: [[u8; 4]; 4] = [
     [0, 0, 0, 0],
     [1, 1, 1, 1],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
 ];
 
-const S_TETRAMINO: [[usize; 4]; 4] = [
+const S_TETRAMINO: [[u8; 4]; 4] = [
     [0, 0, 0, 0],
     [0, 1, 1, 0],
     [1, 1, 0, 0],
     [0, 0, 0, 0],
 ];
 
-const Z_TETRAMINO: [[usize; 4]; 4] = [
+const Z_TETRAMINO: [[u8; 4]; 4] = [
     [0, 0, 0, 0],
     [1, 1, 0, 0],
     [0, 1, 1, 0],
     [0, 0, 0, 0],
 ];
 
-const J_TETRAMINO: [[usize; 4]; 4] = [
-    [0, 0, 0, 0],
-    [0, 1, 0, 0],
-    [1, 1, 1, 0],
-    [0, 0, 0, 0],
-];
-
-const L_TETRAMINO: [[usize; 4]; 4] = [
+const J_TETRAMINO: [[u8; 4]; 4] = [
     [0, 0, 0, 0],
     [1, 0, 0, 0],
     [1, 1, 1, 0],
     [0, 0, 0, 0],
 ];
 
-const O_TETRAMINO: [[usize; 4]; 4] = [
+const L_TETRAMINO: [[u8; 4]; 4] = [
+    [0, 0, 0, 0],
+    [1, 1, 1, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 0],
+];
+
+const O_TETRAMINO: [[u8; 4]; 4] = [
     [0, 0, 0, 0],
     [0, 1, 1, 0],
     [0, 1, 1, 0],
     [0, 0, 0, 0],
 ];
 
-const T_TETRAMINO: [[usize; 4]; 4] = [
+const T_TETRAMINO: [[u8; 4]; 4] = [
     [0, 0, 0, 0],
     [0, 1, 0, 0],
     [1, 1, 1, 0],
     [0, 0, 0, 0],
 ];
 
-enum TetraminoType {
+#[derive(Clone, Copy)]
+pub enum TetraminoType {
     I,
     S,
     Z,
@@ -65,29 +66,43 @@ pub struct TetraminoPlugin;
 
 impl Plugin for TetraminoPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(create_random_tetramino);
+        app.add_startup_system(create_random_tetramino_system)
+            .add_event::<CollidedEvent>()
+            .add_system(on_tetramino_changed)
+            .add_system(keyboard_input);
+        
+        app.add_stage("Tetramino fall", 
+            SystemStage::parallel()
+                    .with_run_criteria(FixedTimestep::step(0.5).with_label("fall"))
+                    .with_system(fall_system)
+        );
+
     }
 }
 
 
-#[derive(Component)]
+#[derive(Component, Clone, Copy)]
 pub struct Tetramino {
-    pub x: usize,
-    pub y: usize,
-    pub shape: [[usize; 4]; 4],
+    pub x: i32,
+    pub y: i32,
+    pub shape: [[u8; 4]; 4],
+    pub tetramino_type: TetraminoType,
 }
+
+struct CollidedEvent;
 
 impl Tetramino {
     pub fn new() -> Self {
         Self { 
-            x: COLS / 2 - 2, 
-            y: ROWS - 4, 
+            x: COLS as i32 / 2 - 2, 
+            y: ROWS as i32 - 4, 
             shape: [
                 [0, 0, 0, 0],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0],
-            ]
+            ],
+            tetramino_type: TetraminoType::I,
         }
     }
 
@@ -111,6 +126,32 @@ impl Tetramino {
         self.shape = new_shape;
     }
 
+    pub fn get_bounds(&self) -> (i32, i32, i32, i32) {
+        let mut min_x: i32 = 3;
+        let mut min_y: i32 = 3;
+        let mut max_x: i32 = 0;
+        let mut max_y: i32 = 0;
+        for x in 0..4 {
+            for y in 0..4 {
+                if self.shape[x as usize][y as usize] == 1 {
+                    if x < min_x {
+                        min_x = x;
+                    }
+                    if x > max_x {
+                        max_x = x;
+                    }
+                    if y < min_y {
+                        min_y = y;
+                    }
+                    if y > max_y {
+                        max_y = y;
+                    }
+                }
+            }
+        }
+        (min_x, min_y, max_x, max_y)
+    }
+
     pub fn set_shape(&mut self, tetramino_type: &TetraminoType) {
         match tetramino_type {
             TetraminoType::I => self.shape = I_TETRAMINO,
@@ -121,10 +162,15 @@ impl Tetramino {
             TetraminoType::O => self.shape = O_TETRAMINO,
             TetraminoType::T => self.shape = T_TETRAMINO,
         }
+        self.tetramino_type = *tetramino_type;
     }
 }
 
-pub fn create_random_tetramino(mut commands: Commands) {
+pub fn create_random_tetramino_system(mut commands: Commands) {
+    create_random_tetramino(&mut commands);
+}
+
+pub fn create_random_tetramino(commands: &mut Commands) {
     let mut rng = rand::thread_rng();
     let tetramino_type = match rng.gen_range(0..7) {
         0 => TetraminoType::I,
@@ -166,7 +212,7 @@ pub fn create_random_tetramino(mut commands: Commands) {
                                 custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                                 ..default()
                             },
-                            transform: Transform::from_translation(Vec3::new(x as f32 * TILE_SIZE, (3 - y) as f32 * TILE_SIZE, 0.0)),
+                            transform: Transform::from_translation(Vec3::new(x as f32 * TILE_SIZE, y as f32 * TILE_SIZE, 0.0)),
                             ..Default::default()
                         });
                     }
@@ -177,6 +223,210 @@ pub fn create_random_tetramino(mut commands: Commands) {
 
 }
 
+fn on_tetramino_changed(mut commands: Commands, 
+                        q_children: Query<(Entity, &Parent, &Sprite)>,
+                        mut q_parent: Query<(Entity, &Tetramino, &mut Transform), Changed<Tetramino>>) {
+    for (tetramino_entity, tetramino, mut transform) in q_parent.iter_mut() {
+        for (entity, parent, _sprite) in q_children.iter() {
+            if parent.0 != tetramino_entity {
+                continue;
+            }
+            commands.entity(entity).despawn();
+        }
+
+        transform.translation = get_coordinate(&tetramino.x, &tetramino.y) + Vec3::new(0.0, 0.0, 0.1);
+
+        let color = match &tetramino.tetramino_type {
+            TetraminoType::I => Color::rgb(0.0, 0.0, 1.0),
+            TetraminoType::S => Color::rgb(1.0, 0.0, 0.0),
+            TetraminoType::Z => Color::rgb(0.0, 1.0, 0.0),
+            TetraminoType::J => Color::rgb(0.0, 1.0, 1.0),
+            TetraminoType::L => Color::rgb(1.0, 1.0, 0.0),
+            TetraminoType::O => Color::rgb(1.0, 0.5, 0.0),
+            TetraminoType::T => Color::rgb(0.5, 0.0, 1.0),
+        };
+
+        commands.entity(tetramino_entity).with_children(|parent| {
+            for x in 0..4 {
+                for y in 0..4 {
+                    if tetramino.shape[x][y] == 1 {
+                        parent.spawn_bundle(SpriteBundle {
+                            sprite: Sprite {
+                                color,
+                                custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                                ..default()
+                            },
+                            transform: Transform::from_translation(Vec3::new(x as f32 * TILE_SIZE, y as f32 * TILE_SIZE, 0.0)),
+                            ..Default::default()
+                        });
+                    }
+                }
+            }
+        });
+    }
+}
+
+fn keyboard_input(
+    keys: Res<Input<KeyCode>>,
+    mut query: Query<(Entity, &mut Tetramino)>,
+    tiles_query: Query<(Entity, &mut Tile)>,
+) {
+    let is_collided_left = check_is_collided(&query,&tiles_query, (-1, 0), 0);
+    let is_collided_right = check_is_collided(&query,&tiles_query, (1, 0), 0);
+    let is_collided_clockwise = check_is_collided(&query,&tiles_query, (0, 0), 1);
+    let is_collided_conterclockwise = check_is_collided(&query,&tiles_query, (0, 0), -1);
+    if keys.just_pressed(KeyCode::D) {
+        for (_, mut tetramino) in query.iter_mut() {
+            if !is_collided_conterclockwise {
+                tetramino.rotate_conterclockwise();
+            }
+        }
+    } else if keys.just_pressed(KeyCode::F) {
+        for (_, mut tetramino) in query.iter_mut() {
+            if !is_collided_clockwise {
+                tetramino.rotate_clockwise();
+            }
+        }
+    }
+    if keys.just_pressed(KeyCode::J) {
+        for (_, mut tetramino) in query.iter_mut() {
+            let (min_x, _, _, _) = tetramino.get_bounds();
+            if tetramino.x > -min_x && !is_collided_left {
+                tetramino.x -= 1;
+            }
+        }
+    } else if keys.just_pressed(KeyCode::K) {
+        for (_, mut tetramino) in query.iter_mut() {
+            let (_, _, max_x, _) = tetramino.get_bounds();
+            if tetramino.x < COLS as i32 - max_x - 1 && !is_collided_right {
+                tetramino.x += 1;
+            }
+        }
+    }
+
+}
+
+fn check_is_collided(tetramino_query: &Query<(Entity, &mut Tetramino)>,
+                     field_query: &Query<(Entity, &mut Tile)>,
+                     delta: (i32, i32),
+                     rotation: i32) -> bool {
+    for (_, tetramino) in tetramino_query.iter() {
+        let (min_x, min_y, max_x, max_y) = tetramino.get_bounds();
+        if delta.1 == -1 && tetramino.y + min_y == 0 {
+            return true;
+        }
+        for x in min_x..max_x + 1 {
+            for y in min_y..max_y + 1 {
+                if tetramino.shape[x as usize][y as usize] == 1 {
+                    match rotation {
+                        0 => {
+                            let tile_x = x + tetramino.x + delta.0;
+                            let tile_y = y + tetramino.y + delta.1;
+                            for (_entity, tile) in field_query.iter() {
+                                if tile.x == tile_x && tile.y == tile_y && tile.value > 0 {
+                                    return true;
+                                }
+                            }
+                        },
+                        -1 => {
+                            let mut tetramino_cloned = tetramino.clone();
+                            tetramino_cloned.rotate_conterclockwise();
+                            let (min_x, min_y, max_x, max_y) = tetramino_cloned.get_bounds();
+                            if tetramino_cloned.y + min_y <= 0 {
+                                return true;
+                            }
+                            if tetramino_cloned.y + max_y >= ROWS as i32 - 1 {
+                                return true;
+                            }
+                            if tetramino_cloned.x + min_x <= 0 {
+                                return true;
+                            }
+                            if tetramino_cloned.x + max_x >= COLS as i32 - 1 {
+                                return true;
+                            }
+                            let tile_x = x + tetramino_cloned.x;
+                            let tile_y = y + tetramino_cloned.y;
+                            for (_entity, tile) in field_query.iter() {
+                                if tile.x == tile_x && tile.y == tile_y && tile.value > 0 {
+                                    return true;
+                                }
+                            }
+                        },
+                        1 => {
+                            let mut tetramino_cloned = tetramino.clone();
+                            tetramino_cloned.rotate_clockwise();
+                            if tetramino_cloned.y + min_y <= 0 {
+                                return true;
+                            }
+                            if tetramino_cloned.y + max_y >= ROWS as i32 - 1 {
+                                return true;
+                            }
+                            if tetramino_cloned.x + min_x <= 0 {
+                                return true;
+                            }
+                            if tetramino_cloned.x + max_x >= COLS as i32 - 1 {
+                                return true;
+                            }
+                            let tile_x = x + tetramino_cloned.x;
+                            let tile_y = y + tetramino_cloned.y;
+                            for (_entity, tile) in field_query.iter() {
+                                if tile.x == tile_x && tile.y == tile_y && tile.value > 0 {
+                                    return true;
+                                }
+                            }
+                        },
+                        _ => {
+                            unreachable!();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+fn on_collided( commands: &mut Commands,
+                tetramino_query: &Query<(Entity, &mut Tetramino)>,
+                field_query: &mut Query<(Entity, &mut Tile)>) {
+    for (entity, tetramino) in tetramino_query.iter() {
+        for (_tile_entity, mut tile) in field_query.iter_mut() {
+            let x = tile.x - tetramino.x;
+            if x < 0 || x >= 4 {
+                continue;
+            }
+            let y = tile.y - tetramino.y;
+            if y < 0 || y >= 4 {
+                continue;
+            }
+            if tetramino.shape[x as usize][y as usize] == 1 {
+                tile.value = match tetramino.tetramino_type {
+                    TetraminoType::I => 1,
+                    TetraminoType::S => 2,
+                    TetraminoType::Z => 3,
+                    TetraminoType::J => 4,
+                    TetraminoType::L => 5,
+                    TetraminoType::O => 6,
+                    TetraminoType::T => 7,
+                }
+            }
+        }
+        create_random_tetramino(commands);
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn fall_system(mut commands: Commands,
+               mut tetramino_query: Query<(Entity, &mut Tetramino)>,
+               mut field_query: Query<(Entity, &mut Tile)>) {
+    if check_is_collided(&tetramino_query, &field_query, (0, -1), 0) {
+        on_collided(&mut commands, &tetramino_query, &mut field_query);
+        return;
+    }
+    for (_entity, mut tetramino) in tetramino_query.iter_mut() {
+        tetramino.y -= 1;
+    }
+}
 
 #[cfg(test)]
 #[test]
@@ -210,6 +460,14 @@ fn test_rotate_clockwise() {
         [0, 0, 1, 0],
         [0, 0, 1, 0],
     ]);
+    tetramino.set_shape(&TetraminoType::S);
+    tetramino.rotate_clockwise();
+    assert_eq!(tetramino.shape, [
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+    ]);
 }
 
 #[cfg(test)]
@@ -224,6 +482,14 @@ fn test_rotate_conterclockwise() {
         [0, 1, 0, 0],
         [0, 1, 0, 0],
     ]);
+    tetramino.set_shape(&TetraminoType::S);
+    tetramino.rotate_conterclockwise();
+    assert_eq!(tetramino.shape, [
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+    ]);
 }
 
 #[cfg(test)]
@@ -232,6 +498,6 @@ fn test_create_tetramino() {
     let mut tetramino = Tetramino::new();
     tetramino.set_shape(&TetraminoType::I);
     assert_eq!(tetramino.shape, I_TETRAMINO);
-    assert_eq!(tetramino.x, COLS / 2 - 2);
-    assert_eq!(tetramino.y, ROWS - 4);
+    assert_eq!(tetramino.x, COLS as i32 / 2 - 2);
+    assert_eq!(tetramino.y, ROWS as i32 - 4);
 }
--- src/tilemap.rs
+++ src/tilemap.rs
@@ -6,20 +6,61 @@ pub const TILE_SIZE: f32 = 20.0;
 
 #[derive(Component)]
 pub struct Tile {
-    pub x: usize,
-    pub y: usize,
+    pub x: i32,
+    pub y: i32,
     pub value: u8,
+}
+
+#[derive(Component)]
+struct Score;
+
+#[derive(Default)]
+pub struct TetrisData {
+    pub score: i32,
 }
 
 pub struct TilemapPlugin;
 
 impl Plugin for TilemapPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(tiles_setup);
+        app.add_startup_system(tiles_setup)
+            .init_resource::<TetrisData>()
+            .add_startup_system(create_score_text)
+            .add_system(on_tile_change)
+            .add_system(burn_the_line)
+            .add_system(update_score_text);
     }
 }
 
-pub fn get_coordinate(x: &usize, y: &usize) -> Vec3 {
+fn create_score_text(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(UiCameraBundle::default());
+    commands
+    .spawn_bundle(TextBundle {
+        text: Text {
+            sections: vec![TextSection {
+                value: "Score: 0".to_string(),
+                style: TextStyle {
+                    font: asset_server.load("font.otf"),
+                    font_size: 20.0,
+                    color: Color::WHITE,
+                },
+            }],
+            ..Default::default()
+        },
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.2)),
+        ..Default::default()
+    }) 
+    .insert(Score);
+}
+
+fn update_score_text(mut commands: Commands, score: Res<TetrisData>, mut score_text: Query<(&mut Text, &Score)>) {
+    let score = score.score;
+    for (mut score_text, _) in score_text.iter_mut() {
+        score_text.sections[0].value = format!("Score: {}", score);
+    }
+}
+
+pub fn get_coordinate(x: &i32, y: &i32) -> Vec3 {
     let mut res = Vec3::new(
         *x as f32 * TILE_SIZE, 
         *y as f32 * TILE_SIZE,
@@ -34,8 +75,8 @@ pub fn get_coordinate(x: &usize, y: &usize) -> Vec3 {
 }
 
 pub fn tiles_setup(mut commands: Commands) {
-    for x in 0..COLS {
-        for y in 0..ROWS {
+    for x in 0..COLS as i32 {
+        for y in 0..ROWS as i32 {
             let tile_position = get_coordinate(&x, &y);
 
             commands.spawn_bundle(SpriteBundle {
@@ -49,6 +90,64 @@ pub fn tiles_setup(mut commands: Commands) {
             })
             .insert(Tile { x, y, value: 0 });
         }
+    }
+}
+
+fn on_tile_change(mut query: Query<(Entity, &Tile, &mut Sprite), Changed<Tile>>) {
+    for (_entity, tile, mut sprite) in query.iter_mut() {
+        sprite.as_mut().color = match tile.value {
+            0 => Color::rgb(0.0, 0.0, 0.0), // nothing
+            1 => Color::rgb(0.0, 0.0, 1.0), // I
+            2 => Color::rgb(1.0, 0.0, 0.0), // S
+            3 => Color::rgb(0.0, 1.0, 0.0), // Z
+            4 => Color::rgb(0.0, 1.0, 1.0), // J
+            5 => Color::rgb(1.0, 1.0, 0.0), // L
+            6 => Color::rgb(1.0, 0.5, 0.0), // O
+            7 => Color::rgb(0.5, 0.0, 1.0), // T
+            _ => unreachable!(),
+        };
+    }
+}
+
+fn burn_the_line(mut tetris_data: ResMut<TetrisData>,
+                 mut query: Query<&mut Tile>) {
+    let mut matrix = vec![vec![0; COLS]; ROWS + 1];
+    for tile in query.iter() {
+        matrix[tile.y as usize][tile.x as usize] = tile.value;
+    }
+    let mut burned = true;
+    let mut count = 0;
+    let mut line = 0;
+    while burned {
+        burned = false;
+        for y in 0..ROWS {
+            let mut line_is_full = true;
+            for x in 0..COLS {
+                if matrix[y][x] == 0 {
+                    line_is_full = false;
+                    break;
+                }
+            }
+            if line_is_full {
+                burned = true;
+                count += 1;
+                line = y;
+                break;
+            }
+        }
+        if burned {
+            for y in line..ROWS {
+                for x in 0..COLS {
+                    matrix[y][x] = matrix[y + 1][x];
+                }
+            }
+        }
+    }
+    if count > 0 {
+        for mut tile in query.iter_mut() {
+            tile.value = matrix[tile.y as usize][tile.x as usize];
+        }
+        tetris_data.score += count * count * 100;
     }
 }
 
